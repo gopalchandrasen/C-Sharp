68a7d8ccd90acd12aff3a84da58e3894a2a8ae94
Gopal Sen <gopal.deshra@gmail.com>
2026-09-19 12:00:16 +0530

    Uploads files

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using System;
+
+// namespace HelloWorld
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       Console.WriteLine("Hello World!");    
+//     }
+//   }
+// }
+
+// static void method(int x, int y)
+// {
+//   if (x > y)
+//   {
+//     Console.WriteLine(x+ " is bigger.");
+//   }
+//   else
+//   {
+//     Console.WriteLine(y+ " is bigger.");
+//   }
+//   Console.Write("The End");  
+// }
+
+// method(Convert.ToInt32(Console.ReadLine()),
+//        Convert.ToInt32(Console.ReadLine())
+//       );
+
+void func(int ?num){
+    if(num!=null){
+        Console.Write(num+ " is the data");
+    }
+    else
+    {
+        Console.Write("Data not provided");
+    }
+}
+
+func(null);
---
+++ exp.cs
@@ -0,0 +1,21 @@
+// void Execute(String targetValue)
+// {
+//     // Implementation of the Execute method
+//     Console.WriteLine("Executing the operation...");
+//     // Additional logic can be added here
+
+//     string actionDetails = string.IsNullOrEmpty(targetValue) ? "" : $"with value['{targetValue}']";
+//     Console.WriteLine($"Action details: {actionDetails}");
+// }
+
+// Console.WriteLine("Enter a target value (or leave empty):");
+// string userInput = Console.ReadLine();
+// Execute(userInput);
+
+void expFunction(string targetValue ="Initial Value")
+{
+    String initialValue=targetValue+" Changed";
+    Console.WriteLine("Executing the operation...");
+    Console.WriteLine($"Target value: {initialValue}");
+}
+expFunction();
